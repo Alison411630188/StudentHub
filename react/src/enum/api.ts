18b00b8b893685d48ce76826aa52e68697e9b81e
TKU-AI-411630188 <112963598+Alison411630188@users.noreply.github.com>
2024-12-18 19:43:23 +0800

    Initial commit

---
+++ react/src/enum/api.ts
@@ -0,0 +1,7 @@
+export enum api {
+    findAll = " http://localhost:2888/api/v1/user/findAll",
+    delete = 'http://localhost:2888/api/v1/user/deletedById',
+    insertOne = 'http://localhost:2888/api/v1/user/insertOne',
+    update = "http://localhost:2888/api/v1/user/updateNameById",
+    findOne = "http://localhost:2888/api/v1/user/findOne"
+}
